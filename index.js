75bb4fc5b6e872f0a215d0afc031743b510cd757
manthan ankolekar <manthan.ank46@gmail.com>
2022-05-17 15:19:56 +0530

    changes made

--- index.js
+++ index.js
@@ -3,8 +3,21 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const path = require('path');
 const passport = require('passport');
-const { mongoose } = require('./db.js');
+//const { mongoose } = require('./db.js');
+const mongoose = require('mongoose');
+const config = require('./config/database');
 
+mongoose.connect(config.database);
+// Connect To Database (OLD CODE)
+//mongoose.connect(config.database, { useMongoClient: true});
+// On Connection
+mongoose.connection.on('connected', () => {
+  console.log('Connected to Database '+config.database);
+});
+// On Error
+mongoose.connection.on('error', (err) => {
+  console.log('Database error '+err);
+});
 var employeeController = require('./controllers/employeeController.js');
 var userController = require('./controllers/usersController.js');
 var todoController = require('./controllers/todoController.js');
@@ -20,6 +33,12 @@ app.use('/user', user)
 
 // Set Static Folder
 app.use(express.static(path.join(__dirname, 'public')));
+
+// Passport Middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+require('./config/passport')(passport);
 
 app.listen(3000, () => console.log('Server started at port : 3000'));
 
